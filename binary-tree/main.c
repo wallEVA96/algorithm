a1b84e3911797e1712a5f784bf0a2f5a2cde3c7c
walleva <walleva@email.com>
2018-09-04 20:48:37 -0700

    Add ALL
    Signed-off-by: walleva <walleva@email.com>

---
+++ binary-tree/main.c
@@ -0,0 +1,36 @@
+/*
+ * main.c
+ *                        _ _                 
+ *         __      ____ _| | | _____   ____ _ 
+ *         \ \ /\ / / _` | | |/ _ \ \ / / _` |
+ *          \ V  V / (_| | | |  __/\ V / (_| |
+ *           \_/\_/ \__,_|_|_|\___| \_/ \__,_|
+ * Copyright (C) 2018 walleva <walleva@ubuntu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+
+struct treeNode;
+typedef struct treeNode *ptr_treeNode;
+typedef struct treeNode {
+	int element;
+	ptr_treeNode left;
+	ptr_treeNode right;
+} *ptr_treeNode;
+
+//struct node {
+//	int index;
+//	struct node *left;
+//	struct node *right;
+//};
+
+int main(){
+	ptr_treeNode tree_node = malloc(sizeof(ptr_treeNode));
+	printf("%ld %ld\n",sizeof(unsigned int),sizeof(struct treeNode));
+
+	
+	return 0;
+}
