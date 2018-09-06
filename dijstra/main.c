6a9d9c946cb2c0d780991c46c120c9771fce68ef
walleva <walleva@email.com>
2018-09-05 23:28:19 -0700

    Add dijstra c

---
+++ dijstra/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *                        _ _                 
+ *         __      ____ _| | | _____   ____ _ 
+ *         \ \ /\ / / _` | | |/ _ \ \ / / _` |
+ *          \ V  V / (_| | | |  __/\ V / (_| |
+ *           \_/\_/ \__,_|_|_|\___| \_/ \__,_|
+ * Copyright (C) 2018 walleva <walleva@ubuntu>
+ * Reference :
+ * https://www.cs.usfca.edu/~galles/visualization/Dijkstra.html
+ * Note:
+ * 1, find cheapest vertex form unknown vertex(that din't update neighbor)
+ * 2, update above vertex's neighbori
+ *		add center vertex's cost with edge cost firstly,
+ *		if calculcating new cost is cheaper than before, instead of that and set the 
+ *		path vertex is the center vertex.
+ * 3, set center vertex is known.
+ * 4 recycle above until there are no cheap vertex or the cheaptest is INF. Abandon!
+ * Distributed under terms of the MIT license.
+ */
+
+
+#include "stdio.h"
+
+int main(){
+
+
+return 0;
+}
