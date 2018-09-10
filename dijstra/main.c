d933e69f95b48a2d161aa4ac0d5d0d244ee92ef1
walleva <walleva@email.com>
2018-09-10 03:13:06 -0700

    Add dijstra main code

--- dijstra/main.c
+++ dijstra/main.c
@@ -10,6 +10,7 @@
  * https://www.cs.usfca.edu/~galles/visualization/Dijkstra.html
  * Note:
  * 1, find cheapest vertex form unknown vertex(that din't update neighbor)
+ *	  it mean that this point will didnt update again.
  * 2, update above vertex's neighbori
  *		add center vertex's cost with edge cost firstly,
  *		if calculcating new cost is cheaper than before, instead of that and set the 
@@ -23,34 +24,56 @@
 #include "stdio.h"
 
 /* define */
-typedef int Vertex;
+struct vertex;
+typedef struct vertex *Vertex_p;
 struct vertex {
-	Vertex vertex;
-	Vertex cost;
-	struct list *next_vertex;
+	int vertex;
+	int cost;
+	Vertex_p next_vertex;
 };
 
-typedef struct vertex *adj_list;
 struct vertex_state{
-	adj_list list;
+	Vertex_p adj_list;
 	int known;
 	int dist;
-	Vertex path;
+	int path;
 };
 
-#define vertex_num 4
-#define INFINITY 10000
-#define FALSE 0
+#define VERTEX_NUM 4
 #define NOPATH 0
+#define INFINITY 10000
 #define TRUE 1
+#define FALSE 0
 
-struct vertex_state vertex_array[vertex_num] = {NULL,
+struct vertex_state vertex_array[VERTEX_NUM] = {NULL,
 												FALSE,
 												INFINITY,
 												NOPATH};
+int minmum(struct vertex_state *vertex_array){
+
+
+}
 
 /* dijstra */
+void dijstra(int vertex, struct vertex_state *vertex_array, size_t vertex_num)
+{
+	int tmp_dist = 0;
+	for(;;){
+		if(vertex == INFINITY)
+				break;
+		vertex_array[vertex].known = TRUE;
+		Vertex_p adj_vertex = vertex_array[vertex].adj_list;
+		while(adj_vertex->next_vertex != NULL){
+			if(vertex_array[adj_vertex->next_vertex->vertex].known != TRUE)
+					tmp_dist = vertex_array[adj_vertex->vertex].dist + adj_vertex->next_vertex->cost;
+					if( tmp_dist < vertex_array[adj_vertex->next_vertex->vertex].dist){
+					vertex_array[adj_vertex->next_vertex->vertex].dist = tmp_dist;
+			}
+			vertex = minmum(vertex_array);
+		}
+	}
 
+}
 /*  main */
 int main(){
 
