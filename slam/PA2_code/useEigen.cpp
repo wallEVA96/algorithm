6d3805f4eaee6a3fcd91e10a5bb818e1bb8c9c77
walleva <1146721668@qq.com>
2018-10-17 23:45:24 +0800

    Matrix Derivative
    Signed-off-by: walleva <1146721668@qq.com>

--- slam/PA2_code/useEigen.cpp
+++ slam/PA2_code/useEigen.cpp
@@ -2,8 +2,8 @@
 #include <ctime>
 using namespace std;
 
-//eigen 
 #include <Eigen/Core>
+// 稠密矩阵的代数运算 逆矩阵，特征值
 #include <Eigen/Dense>
 
 #define MATRIX_SIZE 50
@@ -11,7 +11,9 @@ using namespace std;
 int main(char argc, char **argv){
 	
 	Eigen::Matrix<float,2,3> matrix_23;
+	/* Vertor3D is double 3,1, 三维坐标就相当于矩阵中1行3列 */
 	Eigen::Vector3d v_3d;
+	/* matrxi3d is doule 3,3 */
 	Eigen::Matrix3d matrix_33 = Eigen::Matrix3d::Zero();
 	Eigen::Matrix< double,Eigen::Dynamic,Eigen::Dynamic> matrix_dynamic;
 	Eigen::MatrixXd matrix_x;
