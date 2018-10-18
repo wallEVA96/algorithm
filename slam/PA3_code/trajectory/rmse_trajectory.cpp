4d3761d874895c8d1dbf6aacf7dd9a73cf7b68db
walleva <walleva@email.com>
2018-10-18 02:45:22 -0700

    add RMSE code
    Signed-off-by: walleva <walleva@email.com>

--- slam/PA3_code/trajectory/rmse_trajectory.cpp
+++ slam/PA3_code/trajectory/rmse_trajectory.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 
 // path to trajectory file
-string trajectory_file = "/home/walleva/algorithm/slam/PA3_code/trajectory/trajectory.txt";
+string est_file = "/home/walleva/algorithm/slam/PA3_code/trajectory/estimated.txt";
+string tru_file = "/home/walleva/algorithm/slam/PA3_code/trajectory/groundtruth.txt";
 
 // function for plotting trajectory, don't edit this code
 // start point is red and end point is blue
@@ -22,28 +23,49 @@ int main(int argc, char **argv) {
     /// implement pose reading code
     // start your code here (5~10 lines)
 	cout << "-- Visual SLAM 4 Chapter " << endl;
-	string input_data,line_data;
-	float time;
-	Eigen::Vector3d t;
-	Eigen::Quaterniond q;
-	Sophus::SE3 SE3_tf;
-	ifstream if_trajectory(trajectory_file.c_str());
+	
+	/* define variable */
+	string input_data_est,line_data_est,
+		   input_data_tru,line_data_tru;
+	double time_est = 0,time_tru = 0, rmse = 0;
+	int size = 0;
+	Eigen::Vector3d t_est, t_tru;
+	Eigen::Quaterniond q_est, q_tru;
+	Sophus::SE3 SE3_err, SE3_est, SE3_tru;
+	Eigen::Matrix<double,6,1> se3_err;
+	ifstream if_estimate(est_file.c_str());
+	ifstream if_truth(tru_file.c_str());
 
-	if( !if_trajectory.is_open() ){
+	if( !if_estimate.is_open() || !if_truth.is_open() ){
 		cout << "-- trajectory.txt not found" << endl;
 		return -1;
 	}
-	while( getline(if_trajectory, line_data) ){
-		istringstream iss(line_data);
-		iss >> time >> t[0] >> t[1] >> t[2] >> q.x() >> q.y() >> q.z() >> q.w();
-		SE3_tf = Sophus::SE3(q,t);
-		poses.push_back(SE3_tf);
+
+	while(getline(if_estimate, line_data_est) &&
+		  getline(if_truth,    line_data_tru)){
+		cout << "-- into while" << endl;
+		istringstream iss_est(line_data_est);
+		istringstream iss_tru(line_data_tru);
+		iss_est >> time_est >> t_est[0] >> t_est[1] >> t_est[2] >> q_est.x() >> q_est.y() >> q_est.z() >> q_est.w();
+		iss_tru >> time_tru >> t_tru[0] >> t_tru[1] >> t_tru[2] >> q_tru.x() >> q_tru.y() >> q_tru.z() >> q_tru.w();
+		SE3_est = Sophus::SE3(q_est, t_est);
+		SE3_tru = Sophus::SE3(q_tru, t_tru);
+		SE3_err = SE3_tru.inverse() * SE3_est;
+		se3_err = SE3_err.log();
+		cout << se3_err << endl;
+		rmse += se3_err[0]*se3_err[0]+se3_err[1]*se3_err[1]+se3_err[2]*se3_err[2]+
+				se3_err[3]*se3_err[3]+se3_err[4]*se3_err[4]+se3_err[5]*se3_err[5];
+		size ++;
+		cout << "-- " << rmse << "----" << cout << endl;
+		//poses.push_back(SE3_err);
 	}
-	if( if_trajectory.is_open() )
-		if_trajectory.close();
-    // end your code here
+	cout << std::sqrt(rmse/size) << endl;
+	if( if_estimate.is_open() )
+		if_estimate.close();
+	if( if_truth.is_open() )
+		if_truth.close();
     // draw trajectory in pangolin
-    DrawTrajectory(poses);
+    //DrawTrajectory(poses);
     return 0;
 }
 
