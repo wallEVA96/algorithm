9fa8882ed8d13a5008e2b0005411dd5faba32783
walleva <walleva@email.com>
2018-10-23 02:39:33 -0700

    add tangential distortion
    Signed-off-by: walleva <walleva@email.com>

--- slam/PA4_code/distortion_fun.cpp
+++ slam/PA4_code/distortion_fun.cpp
@@ -29,14 +29,17 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
-	double k1=-0.28,k2=0.074,fx=458.654,fy=457.296;
+	double k1=-0.28,k2=0.074,fx=458.654,fy=457.296,p1=0.00019359, p2=1.76187114e-05;
+	// p1 and p2 is for undistroting tangential. ----()---->
 	for (int v=-200; v<200; v++)
 		for (int u=-200; u<200; u++){
 			double u_temp = u/fx;
 			double v_temp = v/fy; 
 			double r2 = u_temp*u_temp+v_temp*v_temp;
-			double u_dis = u_temp*(1+k1*r2+k2*r2*r2);
-			double v_dis = v_temp*(1+k1*r2+k2*r2*r2);
+			double u_dis = u_temp*(1+k1*r2+k2*r2*r2)+2*p1*u_temp*v_temp+p2*(r2+2*u_temp*u_temp);
+			double v_dis = v_temp*(1+k1*r2+k2*r2*r2)+2*p2*u_temp*v_temp+p1*(r2+2*v_temp*v_temp);
+			//double u_dis = u_temp+2*p1*u_temp*v_temp+p2*(r2+2*u_temp*u_temp);
+			//double v_dis = v_temp+2*p2*u_temp*v_temp+p1*(r2+2*v_temp*v_temp);
 			of_data << u_dis*fx << " " << v_dis*fy << endl;
 	}
 	of_data.close();
