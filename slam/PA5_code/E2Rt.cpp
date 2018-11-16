b81cc5de7908044607f17f165b68124d9008b366
walleva <walleva@email.com>
2018-11-16 01:04:47 -0800

    modify E2Rt code
    Signed-off-by: walleva <walleva@email.com>

--- slam/PA5_code/E2Rt.cpp
+++ slam/PA5_code/E2Rt.cpp
@@ -29,33 +29,44 @@ int main(int argc, char **argv) {
 
     // SVD and fix sigular values
     // START YOUR CODE HERE
-	JacobiSVD<MatrixXd> svd(E,ComputeThinU | ComputeThinV);
+	JacobiSVD<MatrixXd> svd(E, ComputeThinU | ComputeThinV);
 	Matrix3d U = svd.matrixU();
 	Matrix3d V = svd.matrixV();
-	Vector3d d = svd.singularValues();
-	cout << " ===="<< endl << d << endl;
-	//cout << " ++++"<< endl << d.matrix() * U << endl;
-	cout << " ===="<< endl << d.matrix() << endl;
-	cout << " ====" << endl <<U.inverse() * E * V.transpose().inverse() << endl;
+	Matrix3d d = U.inverse() * E * V.transpose().inverse();
+//	Vector3d d = svd.singularValues();
+//	Matrix3d dia;
+//	dia << d[0], 0,    0,
+//			0,   d[1], 0,
+//			0,   0,    d[2];
+//	cout << " ++++"<< endl << dia << endl;
     // END YOUR CODE HERE
 
     // set t1, t2, R1, R2 
     // START YOUR CODE HERE
     Matrix3d t_wedge1;
-    Matrix3d t_wedge2;
-
+    Matrix3d t_wedge2;	
+	AngleAxisd Rz1 = AngleAxisd(M_PI/2, Vector3d::UnitZ());
+	AngleAxisd Rz2 = AngleAxisd(-M_PI/2, Vector3d::UnitZ());
     Matrix3d R1;
     Matrix3d R2;
+
+	t_wedge1 = U * Rz1.matrix() * d * U.transpose();
+    cout << "t1 = " << endl << t_wedge1 << endl;
+	t_wedge2 = U * Rz2.matrix() * d * U.transpose();
+	R1 = U * Rz1.matrix().transpose() * V.transpose();
+	R2 = U * Rz2.matrix().transpose() * V.transpose();
     // END YOUR CODE HERE
 
-    cout << "R1 = " << R1 << endl;
-    cout << "R2 = " << R2 << endl;
-    cout << "t1 = " << Sophus::SO3::vee(t_wedge1) << endl;
-    cout << "t2 = " << Sophus::SO3::vee(t_wedge2) << endl;
+    cout << "R1 = " << endl << R1 << endl;
+    cout << "R2 = " << endl << R2 << endl;
+    cout << "t1 = " << endl << Sophus::SO3::vee(t_wedge1) << endl;
+    cout << "t2 = " << endl << Sophus::SO3::vee(t_wedge2) << endl;
 
     // check t^R=E up to scale
-    Matrix3d tR = t_wedge1 * R1;
-    cout << "t^R = " << tR << endl;
+    cout << "t^R = " << endl <<  t_wedge1 * R1 << endl;
+    cout << "t^R = " << endl <<  t_wedge1 * R2 << endl;
+    cout << "t^R = " << endl <<  t_wedge2 * R1 << endl;
+    cout << "t^R = " << endl <<  t_wedge2 * R2 << endl;
 
     return 0;
 }
