88af7e70a269f704aa91782dd5169f1a5ee88a0d
walleva <1146721668@qq.com>
2018-12-08 23:34:41 +0800

    add G2O qt4 valid code
    Signed-off-by: walleva <1146721668@qq.com>

---
+++ slam/PA2_code/useG2O.cpp
@@ -0,0 +1,17 @@
+/*
+ * useG2O.cpp
+ * Copyright (C) 2018 exbot <exbot@ubuntu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/>
+
+int main(int argc, char **argv){
+
+
+return 1;
+}
+
